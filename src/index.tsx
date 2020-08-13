550bd03d2543935b69c477a1d468d000e4332660
koji-torishima <torikou3685@gmail.com>
2020-08-13 13:48:04 +0900

    画面遷移１

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,71 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useNavigation, NavigationContainer} from '@react-navigation/native';
 
 const styles = StyleSheet.create({
-  conteiner: {
+  conteiner1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  conteiner2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  font1: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  font2: {
+    color: 'red',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
 });
 
-function Index() {
-  return <View style={styles.conteiner} />;
+// 画面1
+function Main() {
+  const {navigate} = useNavigation();
+  return (
+    <View style={styles.conteiner1}>
+      <Text style={styles.font1}>ここはMain画面です</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigate('Sub');
+        }}>
+        <Text style={styles.font2}>sub画面へ遷移</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-export default Index;
+// 画面2
+function Sub() {
+  return (
+    <View style={styles.conteiner2}>
+      <Text style={styles.font1}>Sub</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
+  );
+}
+
+export default function () {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
